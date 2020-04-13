48d18149a68e617debecf7750a0b095b3aa22080
FernandoBritoA <Fernando_BritoA@hotmail.com>
2020-04-12 22:21:07 -0500

    broken our homepage into smaller components, finished styling our menu item component

---
+++ src/Components/Offers/Offers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Offers.scss';
+
+const Offers = () => {
+    return (
+        <div className='offers'>
+            <h1>OFERTAAAS</h1>
+        </div>
+    )
+}
+export default Offers;
